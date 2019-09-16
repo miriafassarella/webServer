e3add3a10d3380a12c8e7e57ec5fdcf58f673f9d
Miriã Fassarella <miriafassarella@gmail.com>
2019-09-16 13:36:50 -0300

    express-validator

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express'); // it is the same as ( const http = require(
 const consign = require('consign');
 const bodyParcer = require('body-parser'); //we use to interpret postman damage
 
+
 let app = express(); //invoking the spress
 
 
 app.use(bodyParcer.urlencoded({ extended: false})); 
 app.use(bodyParcer.json()); //will take the postman data and turn it into json
+
 
 //refers to folder name routes
 consign().include('routes').include('utils').into(app); //include where? into(app).
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 let NeDB = require('nedb');
+const {check, validationResult} = require('express-validator');
 let db = new NeDB({ //stanking the database
-
     //creating the file
     filename: 'users.db', 
     autoload:true //creating the file in memory
@@ -23,19 +23,25 @@ module.exports = (app)=>{
         res.json({
             users //it is the same that users: users
     
-        });
-    
-
-
+                });
             }
         });
+});
+    
+    route.post([check('nome', 'O nome é obrigatório').not().isEmpty(),
+                check('email', 'O email é obrigatório').isEmail(),
+                check('password', 'O password é obrigatório').not().isEmpty(),], 
+    
+    (req, res)=>{
 
-        
-    });
-    
-    route.post((req, res)=>{
-    
-        
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+
+            app.utils.error.send(errors, req, res);
+            return false;
+        }
+
         db.insert(req.body, (err, user)=>{ //entering data into database
 
             if(err){
@@ -43,10 +49,49 @@ module.exports = (app)=>{
             }else{
                 res.status(200).json(user);
                 
+                
             }
-
+            
         });
-    
+});
+
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res)=>{
+
+        db.findOne({_id:req.params.id}).exec((err, user)=>{
+
+            if(err){
+                app.utils.error.send(err, req, res);
+            }else{
+                res.status(200).json(user);
+                
+            }
+         });
     });
 
+    routeId.put((req, res)=>{
+
+        db.update({_id:req.params.id}, req.body, err=>{ 
+            if(err){
+                app.utils.error.send(err, req, res);
+            }else{
+                res.status(200).json(Object.assign(req.body, req.params));
+                
+            }
+        });
+     });
+
+    routeId.delete((req, res)=>{
+
+        db.remove({_id:req.params.id}, {}, err=>{
+
+            if(err){
+                app.utils.error.send(err, req, res);
+            }else{
+                res.status(200).json(req.params);
+                
+            }
+        });
+    });
 };
